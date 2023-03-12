e559a5aee8a9dbfdde2accaf4a6771968824d1cb
mekontso <mekontsoanderson@gmail.com>
2023-03-12 22:37:10 +0100

    Post update, list and delete operations

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,10 +4,9 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -20,7 +19,31 @@ public class PostController {
 
     // Create blog post api
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // get all posts from db
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    // get a post by id
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable(name = "id") Long id) {
+        return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
+    }
+
+    // get a post by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePostById(@PathVariable(name = "id") Long id) {
+        postService.deletePostById(id);
+        return ResponseEntity.ok("Success delete");
     }
 }
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -1,10 +1,14 @@
 package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service // make a class as service
 public class PostServiceImpl implements PostService {
@@ -17,10 +21,61 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto createPost(PostDto postDto) {
         // Convert Dto to entity
-        Post post = new Post(postDto.getTitle(), postDto.getDescription(), postDto.getContent());
+        Post post = mapToEntity(postDto);
         //persist entity
         post = postRepository.save(post); // save inDB
         // Convert entity to dto
-        return new PostDto(post.getId(), post.getTitle(), postDto.getDescription(), post.getContent());
+        return mapToDTO(post);
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        var posts = postRepository.findAll();
+        return posts.stream().map(this::mapToDTO).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public PostDto getPostById(Long id) {
+        // get the post, return if found or throw exception
+        return mapToDTO(postRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Post","id",String.valueOf(id))));
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, Long id) {
+        // search post in db
+        var post = postRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Post","id",String.valueOf(id)));
+        // update fields
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        // update db
+        postRepository.save(post);
+        return mapToDTO(post);
+    }
+
+    @Override
+    public void deletePostById(Long id) {
+        // search post in db
+        var post = postRepository.findById(id).orElseThrow(() ->new ResourceNotFoundException("Post","id",String.valueOf(id)));
+        postRepository.delete(post);
+    }
+
+    /**
+     * Converts post to post DTO
+     * @param post object to convert
+     * @return post DTO
+     */
+    private PostDto mapToDTO(Post post){
+        return new PostDto(post.getId(),post.getTitle(),post.getDescription(),post.getContent());
+    }
+
+    /**
+     * Converts post DTO to post
+     * @param postDto post dto to convert
+     * @return post converted
+     */
+    private Post mapToEntity(PostDto postDto){
+        return new Post(postDto.getTitle(), postDto.getDescription(), postDto.getContent());
     }
 }
